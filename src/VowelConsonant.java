cd11e2be614b81986e6bcb34d99d50f971c6d088
Annu <annusinghraj1998@gmail.com>
2022-05-26 00:09:35 +0530

    Vowel or Consonant Commit

---
+++ src/VowelConsonant.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class VowelConsonant {
+    public static void main(String[] args) {
+        char vowel[]= {'a','e','i','o','u','A','E','I','O','U'};
+        Scanner s1 = new Scanner(System.in);
+        System.out.print("Input a character: ");
+        char c = s1.next().charAt(0);
+        System.out.println("You have entered "+c);
+        for(int i=0; i<=9; i++)
+        {
+            if(c==vowel[i]) {
+                System.out.println("Vowel");
+                System.exit(0);
+            }
+        }
+        System.out.println("consonant");
+
+    }
+}
