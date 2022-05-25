71a59236dae2da475c64ae75e9e05023e8bc3ae2
Annu <annusinghraj1998@gmail.com>
2022-05-25 22:34:43 +0530

    Harmonic Number Commit

---
+++ src/HarmonicNumber.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+public class HarmonicNumber {
+    public static void main(String[] args) {
+        int num;
+        float res=0;
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter the positive number greater than 0");
+        num=s.nextInt();
+        if(num>0)
+        {
+            for(float i=1;i<=num;i++)
+            {
+                System.out.print(1+"/"+(int)i+"\t");
+                res+=1/i;
+            }
+            System.out.println();
+            System.out.println("result is "+res);
+        }
+        else
+            System.out.println("Enter valid number");
+    }
+}
+
+
+
